7821afc791da62e8ea7210c1108cbcc5a438c057
Brett Pilcher <brettpilch@yahoo.com>
2015-10-06 18:54:54 -0400

    update GUI to work with site selection

--- nflstatsGUI.py
+++ nflstatsGUI.py
@@ -15,26 +15,31 @@ DEPENDENCIES:
     nflgame (pip install nflgame) or (https://github.com/BurntSushi/nflgame)
 """
 
-from nflstats import League, PASSING_STATS, RUSHING_STATS, RATE_STATS, default, parse_seq
+from nflstats import League, parse_seq
 import Tkinter as gui
 import nflgame as ng
 
-ALL_STATS = PASSING_STATS + RUSHING_STATS
-
-def get_results(league, team, year, week, cum, rate, widget):
+def get_results(team, year, week, site, cum, rate, widget):
     if team.get() == "All teams":
         thisteam = ng.teams
     else:
-        thisteam = default(parse_seq(team.get(), False), list(ng.teams))
+        thisteam = parse_seq(team.get(), [team[0] for team in ng.teams],
+                             [team[0] for team in ng.teams], False)
     if week.get() == "All weeks":
-        thisweek = default(parse_seq(None), list(range(1, 18)))
+        thisweek = parse_seq(None, list(range(1, 18)), list(range(1, 18)))
     else:
-        thisweek = default(parse_seq(week.get()), list(range(1, 18)))
+        thisweek = parse_seq(week.get(), list(range(1, 18)),
+                             list(range(1, 18)))
     if year.get() == "All years":
-        thisyear = default(parse_seq(None), list(range(2009, 2015)))
+        thisyear = parse_seq(None, [2013, 2014], list(range(2009, 2015)))
     else:
-        thisyear = default(parse_seq(year.get()), list(range(2009, 2015)))
-    league = League(thisyear, thisweek, thisteam, cum.get(), rate.get())
+        thisyear = parse_seq(year.get(), [2013, 2014], list(range(2009, 2015)))
+    if site.get() == "All sites":
+        thissite = parse_seq(None, ['home', 'away'], ['home', 'away'], False)
+    else:
+        thissite = parse_seq(site.get(), ['home', 'away'],
+                             ['home', 'away'], False)
+    league = League(thisyear, thisweek, thisteam, thissite, cum.get(), rate.get())
     league.compile()
     widget.delete(1.0, gui.END)
     widget.insert(gui.END, str(league))
@@ -43,9 +48,9 @@ def get_results(league, team, year, week, cum, rate, widget):
 def runGUI():
     app = gui.Tk()
     app.title("NFL Team Stats Query")
-    app.geometry("1200x800+10+10")
+    app.geometry("1300x800+10+10")
 
-    display_text = gui.Text(app, width=150)
+    display_text = gui.Text(app, width=200)
     display_text.insert(gui.END, "Select parameters below.")
     display_text.pack()
     
@@ -64,16 +69,24 @@ def runGUI():
     week_entry = gui.Entry(app, textvariable = week_var)
     week_entry.pack()
 
+    site_var = gui.StringVar()
+    site_var.set("All sites")
+    site_entry = gui.Entry(app, textvariable = site_var)
+    site_entry.pack()
+
     cum_var = gui.IntVar()
-    cum_button = gui.Checkbutton(app, text="Show cumulative Stats", variable=cum_var)
+    cum_button = gui.Checkbutton(app, text="Show Cumulative Stats", 
+                                 variable=cum_var)
     cum_button.pack()
 
     rate_var = gui.IntVar()
-    rate_button = gui.Checkbutton(app, text="Show Rate Stats", variable=rate_var)
+    rate_button = gui.Checkbutton(app, text="Show Rate Stats",
+                                  variable=rate_var)
     rate_button.pack()
 
     button1 = gui.Button(app, text = 'Get Stats', width = 40,
-        command = lambda: get_results(team_var, year_var, week_var, cum_var, rate_var, display_text))
+        command = lambda: get_results(team_var, year_var, week_var, site_var,
+                                      cum_var, rate_var, display_text))
     button1.pack()
 
     app.mainloop()
